8975030f5637c7364093ebe9923139a0f5defd05
grasshousedev <finnelyhawthorne@gmail.com>
2019-03-03 21:33:17 +0000

    Split dashboard menu body header to a Component

---
+++ src/components/dashboard-menu/DashboardMenuBodyHeader.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class DashboardMenuBodyHeader extends Component {
+    constructor(props) {
+        super(props);
+
+        this.queryInputRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.focusQueryInput();
+    }
+
+    componentDidUpdate() {
+        this.focusQueryInput();
+    }
+
+    focusQueryInput = () => {
+        setTimeout(() => {
+            this.queryInputRef.current && this.queryInputRef.current.focus();
+        }, 5);            
+    }
+
+    render() {
+        const { query, handleChangeQuery, toggleBodyOpen } = this.props;
+
+        return <div className="dashboard-menu__body-header">
+            <div className="dashboard-menu__body-query">
+                <input ref={this.queryInputRef}
+                    type="text" value={query} placeholder="Type your query here..."
+                    className="dashboard-menu__body-query__input"
+                    onChange={handleChangeQuery} />
+                <i className="dashboard-menu__body-header__icon dashboard-menu__body-header__icon-search fas fa-search" />
+            </div>
+            <div className="dashboard-menu__body-controls">
+                <i className="fas fa-times dashboard-menu__body-controls__control" onClick={() => toggleBodyOpen(false)}></i>
+            </div>
+        </div>;
+    }
+};
+
+DashboardMenuBodyHeader.propTypes = {
+    handleChangeQuery: PropTypes.func.isRequired,
+    toggleBodyOpen: PropTypes.func.isRequired,
+    query: PropTypes.string,
+};
