b6e37fd11e79a481566c6f4862126764505cf144
grasshousedev <finnelyhawthorne@gmail.com>
2019-03-10 12:12:14 +0000

    Use Lorem component for headers showcase and home

--- src/components/style/Headers.js
+++ src/components/style/Headers.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Lorem } from 'components/ui/Lorem';
 
 export function Headers() {
     const [showHeaders, setShowHeaders] = useState(true);
@@ -6,20 +7,35 @@ export function Headers() {
     return <Fragment>
         <h2 onClick={() => setShowHeaders(!showHeaders)} style={{ cursor: 'pointer' }}>Headers</h2>
         {showHeaders && <Fragment>
-            <div className="flex-container">
-                <div className="w-300"><h1>h1 Title</h1></div>
+            <div className="ui-section">
+                <div className="ui-section__text">
+                    <h1>h1 Title</h1>
+                    <Lorem />
+                </div>
             </div>
-            <div className="flex-container">
-                <div className="w-300"><h2>h2 Title</h2></div>
+            <div className="ui-section">
+                <div className="ui-section__text">
+                    <h2>h2 Title</h2>
+                    <Lorem />
+                </div>
             </div>
-            <div className="flex-container">
-                <div className="w-300"><h3>h3 Title</h3></div>
+            <div className="ui-section">
+                <div className="ui-section__text">
+                    <h3>h3 Title</h3>
+                    <Lorem />
+                </div>
             </div>
-            <div className="flex-container">
-                <div className="w-300"><h4>h4 Title</h4></div>
+            <div className="ui-section">
+                <div className="ui-section__text">
+                    <h4>h4 Title</h4>
+                    <Lorem />
+                </div>
             </div>
-            <div className="flex-container">
-                <div className="w-300"><h5>h5 Title</h5></div>
+            <div className="ui-section">
+                <div className="ui-section__text">
+                    <h5>h5 Title</h5>
+                    <Lorem />
+                </div>
             </div>
         </Fragment>}
     </Fragment>;
