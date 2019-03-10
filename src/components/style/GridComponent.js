fcab51753dc585465a7d62e8247699dd3e7010b1
grasshousedev <finnelyhawthorne@gmail.com>
2019-03-10 09:53:17 +0000

    Improved grid showcase with height, width and code highlight

--- src/components/style/GridComponent.js
+++ src/components/style/GridComponent.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from 'react';
-
-import { Grid } from '../grid/Grid';
+import { Grid } from 'components/grid/Grid';
+import { CodeHighlight } from 'components/style/CodeHighlight';
 
 const rows = [];
 for (let i=0; i<1000; i++) {
@@ -11,6 +11,9 @@ for (let i=0; i<1000; i++) {
         test3: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas semper leo ac porta. Integer metus urna, lacinia vitae purus et, pharetra auctor purus. Vivamus bibendum id lacus sit amet faucibus. Etiam tortor orci, varius at massa ut, tristique suscipit lorem. Nullam vel metus ex. Morbi vitae mauris volutpat erat commodo ultricies at ac magna. Cras condimentum id magna vitae blandit. Vestibulum auctor, magna porta sollicitudin egestas, elit quam interdum massa, consequat sollicitudin dui felis nec massa. Nullam aliquet velit eget metus placerat tempus.`,
         test4: `Short column for row ${i}`,
         test5: `Mid col row ${i}`,
+        test6: `Test field test2`,
+        test7: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis egestas semper leo ac porta. Integer metus urna, lacinia vitae purus et, pharetra auctor purus. Vivamus bibendum id lacus sit amet faucibus. Etiam tortor orci, varius at massa ut, tristique suscipit lorem. Nullam vel metus ex. Morbi vitae mauris volutpat erat commodo ultricies at ac magna. Cras condimentum id magna vitae blandit. Vestibulum auctor, magna porta sollicitudin egestas, elit quam interdum massa, consequat sollicitudin dui felis nec massa. Nullam aliquet velit eget metus placerat tempus.`,
+        test8: `Short column for row ${i}`,
     });
 }
 
@@ -29,18 +32,41 @@ export function GridComponent() {
     const [sectionBorder, setSectionBorder] = useState(true);
     const [pinnedLeft, setPinnedLeft] = useState(['id']);
     const [pinnedRight, setPinnedRight] = useState([]);
+    const [width, setWidth] = useState();
+    const [height, setHeight] = useState(300);
 
-    const columns = ['id', 'test1', 'test2', 'test3', 'test4', 'test5'];
+    const columns = ['id', 'test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8'];
     const columnsLabel = { id: 'ID', test1: 'Test 1', test2: 'Test 2', test3: 'Test 3', test4: 'Test 4', test5: 'Test 5' };
+
+    const gridOptions = {
+        columns, columnsLabel,
+        columnsWidth: { id: 40, test7: 400 },
+        width, height,
+        pinnedColumns: { left: pinnedLeft, right: pinnedRight },
+        styles: { zebra, rowBorder, sectionBorder },
+    };
 
     return <div>
         <div className="ui-section">
-            <button className={`button button--small ${showControls ? 'button--primary' : '' }`} onClick={() => setShowControls(!showControls) }>Show Controls</button>
+            <button className={`button button--small`} onClick={() => setShowControls(!showControls) }>Toggle show Controls</button>
         </div>
         {showControls && <Fragment>            
             <div className="ui-section"> 
                 <div className="ui-section-column w-50pc">
-                    <h3 className="ui-title">Styles</h3>
+                    <h2 className="ui-title">Sizes</h2>
+                    <div className="ui-section ui-form-container">
+                        <div className="ui-form-field">
+                            <div className="ui-form-label w-100">Width</div>
+                            <div className="ui-form-input">
+                                <input value={width || ''} onChange={e => setWidth(e.target.value ? +e.target.value : undefined)} />
+                            </div>
+                        </div>
+                        <div className="ui-form-field">
+                            <div className="ui-form-label w-100">Height</div>
+                            <div className="ui-form-input"><input value={height || ''} onChange={e => setHeight(e.target.value ? +e.target.value : undefined)} /></div>
+                        </div>
+                    </div>
+                    <h2 className="ui-title">Styles</h2>
                     <div className="ui-section">
                         <button className={`button button--small ${zebra ? 'button--positive' : ''}`} onClick={() => setZebra(!zebra) }>Zebra</button>
                         <button className={`button button--small ${rowBorder ? 'button--positive' : ''}`} onClick={() => setRowBorder(!rowBorder) }>Row Border</button>
@@ -48,27 +74,27 @@ export function GridComponent() {
                     </div>
                 </div>
                 <div className="ui-section-column w-50pc">
-                    <h3 className="ui-title">Pinned Columns</h3>
+                    <h2 className="ui-title">Pinned Columns</h2>
                     <div className="ui-section">
                         <div className="ui-section-column w-50pc">
-                            <h4 className="ui-title">Left</h4>
+                            <h4 className="ui-title ui-section-title">Left</h4>
                             {columns.map(column => {
                                 return <div key={`pinned-left-${column}`}>
                                     <input
                                         type="checkbox" id={`pin-column-left-${column}`}
                                         onChange={e => setPinnedLeft(pinColumn(column, e.target.checked, pinnedLeft))} checked={pinnedLeft.includes(column)} />
-                                    <label htmlFor={`pin-column-left-${column}`}>{columnsLabel[column]}</label>
+                                    <label htmlFor={`pin-column-left-${column}`}>{columnsLabel[column] ? columnsLabel[column] : column}</label>
                                 </div>;
                             })}                    
                         </div>
                         <div className="ui-section-column w-50pc">
-                            <h4 className="ui-title">Right</h4>
+                            <h4 className="ui-title ui-section-title">Right</h4>
                             {columns.map(column => {
                                 return <div key={`pinned-right-${column}`}>
                                     <input
                                         type="checkbox" id={`pin-column-right-${column}`}
                                         onChange={e => setPinnedRight(pinColumn(column, e.target.checked, pinnedRight))} checked={pinnedRight.includes(column)} />
-                                    <label htmlFor={`pin-column-right-${column}`}>{columnsLabel[column]}</label>
+                                    <label htmlFor={`pin-column-right-${column}`}>{columnsLabel[column] ? columnsLabel[column] : column}</label>
                                 </div>;
                             })}                    
                         </div>           
@@ -78,12 +104,15 @@ export function GridComponent() {
         </Fragment>}
 
         <Grid
-            columns={columns}
-            columnsLabel={columnsLabel}
-            columnsWidth={{ id: 40 }}
+            {...gridOptions}
             rows={rows}
-            pinnedColumns={{ left: pinnedLeft, right: pinnedRight }}
-            styles={{ zebra, rowBorder, sectionBorder }}
         />
+
+        <div>
+            <h2 className="ui-title">Generated <em>props</em></h2>
+            <CodeHighlight>
+                {JSON.stringify({ ...gridOptions, rows: [] }, null, 4)}
+            </CodeHighlight>
+        </div>
     </div>;
 }
