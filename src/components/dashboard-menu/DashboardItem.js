c6a22b6d08c24c99ff1b04deb562809e4261c655
grasshousedev <finnelyhawthorne@gmail.com>
2019-03-16 11:55:34 +0000

    Added showQueryInput prop to dashboard menu item

--- src/components/dashboard-menu/DashboardItem.js
+++ src/components/dashboard-menu/DashboardItem.js
@@ -2,15 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class DashboardItem extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            showQueryBar: true,
-            showQueryPrefix: true,
-        };
-    }
-
     render() {
         return <div>This render should be overridden!</div>;
     }
