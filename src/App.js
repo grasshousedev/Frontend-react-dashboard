5615fc99eccb546f32a156ed8713b7a6ff35f922
grasshousedev <finnelyhawthorne@gmail.com>
2019-03-18 22:06:30 +0000

    Creatd a global level loading and initialized flags

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './App.scss';
 
+import { SET_APPLICATION_INITIALIZED } from 'store/actions';
 import { Dashboard } from './Dashboard';
 
 export default function App ({ store }) {
+    useEffect(() => {
+        store.dispatch({ type: SET_APPLICATION_INITIALIZED, initialized: true });
+    }, []);
+
     return (
         <Provider store={store}>
             <BrowserRouter>
---
+++ src/store/actions.js
@@ -0,0 +1,4 @@
+// Set the application as initialized, should be done once
+export const SET_APPLICATION_INITIALIZED = 'SET_APPLICATION_INITIALIZED';
+// Set the global flag for the application loading
+export const SET_APPLICATION_LOADING = 'SET_APPLICATION_LOADING';
