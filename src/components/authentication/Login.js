58a19708a5ceb37afd659f1d3b87c0e56a876349
grasshousedev <finnelyhawthorne@gmail.com>
2019-03-18 08:43:56 +0000

    Made login form pluggable

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import { authenticationService } from 'libs/authentication/authentication';
 import { withAuthentication } from 'libs/authentication/storeConnection';
 
+import { BaseLogin } from './BaseLogin';
+
 class Login extends Component {
     constructor(props) {
         super(props);
@@ -40,60 +42,41 @@ class Login extends Component {
     }
 
     render() {
-        const { authentication } = this.props;
+        const { authentication, baseLoginComponent } = this.props;
         const { isLoading, username, password, error } = this.state;
 
         if (authentication.loggedIn) { 
-            return <div className="page-body__container">
-                <h2>Hello {authentication.user.first_name}, welcome!</h2>
-                <div className="ui-section">
-                    <div className="ui-section__column">
-                        <div>You are logged in.</div>
-                    </div>
-                    <div className="ui-section__column w200">
-                        <button className="button button--negative"
-                            onClick={authenticationService.logout}>Logout</button>
-                    </div>
+            return  <div className="ui-section">
+                <div className="ui-section__column">
+                    <div>You are logged in.</div>
+                </div>
+                <div className="ui-section__column w200">
+                    <button className="button button--negative"
+                        onClick={authenticationService.logout}>Logout</button>
                 </div>
             </div>;
         }
 
-        return <div className="page-body__container">            
-            <h2>Login</h2>
+        const LoginRenderComponent = baseLoginComponent ? baseLoginComponent : BaseLogin;
+
+        return <div>           
             {/* eslint-disable-next-line */}
             <form action="javascript:void(0)">
-                <div className="ui-form__container ui-form__container--w100">
-                    <div className="ui-form__field">
-                        <div className="ui-form__label">Username</div>
-                        <div className="ui-form__input">
-                            <input type="text" onChange={(e) => this.updateField('username', e.target.value)} value={username} disabled={isLoading} />
-                        </div>
-                    </div>
-                    <div className="ui-form__field">
-                        <div className="ui-form__label">Password</div>
-                        <div className="ui-form__input">
-                            <input type="password" onChange={(e) => this.updateField('password', e.target.value)} value={password} disabled={isLoading} />
-                        </div>
-                    </div>
-                    <div>
-                        <button className="button button--positive"
-                            onClick={this.login}>Login</button>
-                        {isLoading && <i className="fas fa-spinner fa-pulse"></i>}
-                    </div>
-                </div>
+                <LoginRenderComponent
+                    username={username} password={password}
+                    isLoading={isLoading}
+                    login={this.login}
+                    updateField={this.updateField}
+                    error={error}
+                />
             </form>
-            {error && 
-                <div>
-                    <h4>Error</h4>
-                    <div>{error.status}: {error.statusText}</div>
-                </div>
-            }
         </div>;
     }
 };
 
 Login.propTypes = {
     authentication: PropTypes.object,
+    baseLoginComponent: PropTypes.node,
 };
 
 const connectedLogin = withAuthentication(Login);
