cd4b9002a49c16d5becae3bea290b1589b98a26b
grasshousedev <finnelyhawthorne@gmail.com>
2019-03-18 09:24:09 +0000

    Added a full screen login

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,12 +4,23 @@ import PropTypes from 'prop-types';
 import { withAuthentication } from 'libs/authentication/storeConnection';
 import { Header } from './scaffold/Header/Header';
 import { Routes } from 'routes/Routes';
+import { Login } from 'components/authentication/Login';
+import { DashboardLogin } from 'components/authentication/DashboardLogin';
 
 
 function Dashboard({ authentication }) {
     if (!authentication.storageLoginAttempt)
-        return <div></div>;
-        
+        return <div className="ui-loader__preloader__container">
+            <div className="ui-preloader__loader ui-loader ui-loader--large" />
+        </div>;
+
+    if (!authentication.loggedIn)
+        return <div className="dashboard__full-screen__login__container">
+            <div className="dashboard__full-screen__login__form">
+                <Login loginComponent={DashboardLogin} />
+            </div>
+        </div>;
+
     return <div>
         <Header />
         <div className="dashboard-content">
