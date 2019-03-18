29ac804a793c8de57ebcab84c6b0e8ae08fc7087
grasshousedev <finnelyhawthorne@gmail.com>
2019-03-18 15:29:44 +0000

    Added finance landing page

---
+++ src/applications/finance/FinanceLanding.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+export function FinanceLanding() {    
+    return <Fragment>
+        <div className="dashboard-ui__page-body__container">
+            <h2>Finance</h2>
+            <p>This is the landing page of Finance module.</p>
+        </div>
+    </Fragment>;
+}
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,12 +4,14 @@ import { Route, Switch } from "react-router-dom";
 import { Home } from 'components/Home';
 import { Landing as MachineLearningLanding } from 'applications/machine-learning/Landing';
 import { StyleShowcase } from 'applications/style-showcase/StyleShowcase';
+import { FinanceLanding } from 'applications/finance/FinanceLanding';
 
 export class Routes extends Component {
 
     render() {
         return <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/apps/finance" component={FinanceLanding} />
             <Route exact path="/machine-learning" component={MachineLearningLanding} />
             <Route exact path="/style-showcase" component={StyleShowcase} />
         </Switch>;        
