c85d9dfad92e0c0420e00f2649c11484b5d91d4a
grasshousedev <finnelyhawthorne@gmail.com>
2019-03-21 09:01:24 +0000

    Added withRouter to  Routes and convert to func

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch, withRouter } from "react-router-dom";
 
 import { Home } from 'components/Home';
 import { Landing as MachineLearningLanding } from 'applications/machine-learning/Landing';
 import { StyleShowcase } from 'applications/style-showcase/StyleShowcase';
 import { FinanceLanding } from 'applications/finance/FinanceLanding';
 
-export class Routes extends Component {
-
-    render() {
-        return <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/apps/finance" component={FinanceLanding} />
-            <Route path="/machine-learning" component={MachineLearningLanding} />
-            <Route path="/style-showcase" component={StyleShowcase} />
-        </Switch>;        
-    }
+function Routes() {
+    return <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/apps/finance" component={FinanceLanding} />
+        <Route path="/machine-learning" component={MachineLearningLanding} />
+        <Route path="/style-showcase" component={StyleShowcase} />
+    </Switch>;        
 }
+
+const ConnectedRoutes = withRouter(Routes);
+export { ConnectedRoutes as Routes };
