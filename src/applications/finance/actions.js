a891d08b8da8e41bc612086823f14b3bb5f8cce6
grasshousedev <finnelyhawthorne@gmail.com>
2019-04-02 22:09:28 +0100

    Added moneyMovement entity and reducers

--- src/applications/finance/actions.js
+++ src/applications/finance/actions.js
@@ -10,3 +10,12 @@ export const SET_CATEGORY = 'SET_CATEGORY';
 // Remove a single category
 export const DELETE_CATEGORY = 'DELETE_CATEGORY';
 
+// Set a list of money movements
+export const SET_MONEY_MOVEMENTS = 'SET_MONEY_MOVEMENTS';
+
+// Set a single money movement
+export const SET_MONEY_MOVEMENT = 'SET_MONEY_MOVEMENT';
+
+// Remove a single money movement
+export const DELETE_MONEY_MOVEMENT = 'DELETE_MONEY_MOVEMENT';
+
