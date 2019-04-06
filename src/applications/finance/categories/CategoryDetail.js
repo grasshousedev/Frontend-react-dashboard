bd343c7e7236efd499bc6eb5e0589af5c0899895
grasshousedev <finnelyhawthorne@gmail.com>
2019-04-06 11:10:09 +0100

    Added MoneyMovementsGrid component

--- src/applications/finance/categories/CategoryDetail.js
+++ src/applications/finance/categories/CategoryDetail.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 
-import { Grid } from 'components/grid/Grid';
 import { FullSectionLoader } from 'components/ui/Loader';
 import { PageHeader } from 'components/ui/PageHeader';
 
@@ -10,6 +9,7 @@ import { withFinance } from '../storeConnection';
 import { MoneyMovementEntity } from '../models/moneyMovement';
 import { FINANCE_BASE_URL } from '../FinanceLanding';
 import { CategoryLineChart, CategoryMonthlyChart } from './CategoryCharts';
+import { MoneyMovementsGrid } from '../moneyMovements/MoneyMovementsGrid';
 
 
 function CategoryDetail({ match, finance }) {
@@ -38,17 +38,7 @@ function CategoryDetail({ match, finance }) {
     const controls = <Link
         to={`${FINANCE_BASE_URL}/categories/${category.id}/edit`}
         className="ui-button ui-button--primary"
-    >Edit</Link>;
-
-    const mmColumns = ['movement_icon', 'amount', 'movement_date', 'description', 'tags', 'id'];
-    const mmColumnsLabel = { movement_icon: '', amount: 'Amount', movement_date: 'Date', description: 'Description', tags: 'Tags', id: 'ID' };
-    const mmColumnsWidth = { movement_icon: 20, amount: 70, movement_date: 80, description: 400, tags: 200, id: 20 };
-    const mmRows = Object.values(moneyMovements)
-        .sort((mm1, mm2) => mm1.movement_date > mm2.movement_date ? -1 : 1)
-        .map(mm => ({
-            ...mm,
-            movement_icon: mm.movement === '-' ? <i className="fas fa-arrow-down red" /> : <i className="fas fa-arrow-up teal" />
-        }));
+    >Edit</Link>;    
 
     return <div>
         <PageHeader controls={controls}>
@@ -63,15 +53,7 @@ function CategoryDetail({ match, finance }) {
         <div className="ui-page-body">
             <CategoryLineChart category={category} moneyMovements={moneyMovements} chartStyle={{ marginBottom: '1rem' }} />
             <CategoryMonthlyChart category={category} moneyMovements={moneyMovements} chartStyle={{ marginBottom: '1rem' }} />
-            <Grid
-                rows={mmRows}
-                columns={mmColumns}
-                columnsWidth={mmColumnsWidth}
-                columnsLabel={mmColumnsLabel}
-                customCellClass={{ amount: 'finance__money-movement__amount-cell' }}
-                autoHeightRows={mmRows.length > 10 ? 10 : mmRows.length + 2}
-                height={null}
-            />
+            <MoneyMovementsGrid moneyMovements={moneyMovements} />
         </div>
     </div>;
 
