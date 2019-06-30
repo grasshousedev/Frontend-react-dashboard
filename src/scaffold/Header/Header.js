da70384ebb9602ffd31e0f116a79f805c4aead45
grasshousedev <finnelyhawthorne@gmail.com>
2019-06-30 18:45:54 +0100

    Refactored Header into smaller components

--- src/scaffold/Header/Header.js
+++ src/scaffold/Header/Header.js
@@ -1,93 +1,12 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { DashboardMenu } from 'components/dashboard-menu/DashboardMenu';
-import { DashboardItem } from 'components/dashboard-menu/DashboardItem';
 
-import { Login } from 'components/authentication/Login';
-import { withAuthentication } from 'libs/authentication/storeConnection';
-import { generateTags, hasTag } from './headerUtils';
-
-class Applications extends DashboardItem {
-    applications = [
-        {
-            name: 'Finance',
-            tags: generateTags('Finance'),
-            icon: <i className="fas fa-file-invoice-dollar"></i>,
-            link: '/apps/finance'
-        },
-        {
-            name: 'Machine Learning',
-            tags: generateTags('Machine Learning'),
-            icon: <i className="fas fa-cogs"></i>,
-            link: '/machine-learning'
-        },
-        {
-            name: 'Styles Showcase',
-            tags: generateTags('Styles Showcase'),
-            icon: <i className="fas fa-palette"></i>,
-            link: '/style-showcase'
-        }
-    ]
-    
-    navigate = (path) => {
-        const { history, resetDashboardMenu } = this.props;
-        
-        history.push(path);
-        resetDashboardMenu();
-    }
-
-    render() {
-        const { query } = this.props;
-
-        return <div className="ui-tiles__container ui-tiles">
-            {this.applications.map(application => {
-                if (hasTag(application.tags, query)) {
-                    return <div                        
-                        className="ui-tiles__tile"
-                        key={application.name}
-                        onClick={() => this.navigate(application.link)}
-                    >
-                        <div className="ui-tiles__tile__icon">
-                            {application.icon}
-                        </div>
-                        <div className="ui-tiles__tile__label">
-                            {application.name}
-                        </div>
-                    </div>;
-                }
-                return <Fragment key={application.name} />;
-            })}
-        </div>;
-    }
-
-}
-
-Applications.propTypes = {
-    query: PropTypes.string,
-    resetDashboardMenu: PropTypes.func,
-    history: PropTypes.object,
-};
-
-const ApplicationsRouter = withRouter(Applications);
-
-class User extends DashboardItem {
-    render() {
-        const { authentication } = this.props;
-
-        return <div className="ui-page-body">
-            {authentication.loggedIn && <h2>Hello {authentication.user.first_name}, welcome!</h2>}
-            {!authentication.loggedIn && <h2>Login</h2>}
-            <Login />
-        </div>;
-    }
-}
-User.propTypes = {
-    authentication: PropTypes.object,
-};
-const connectedUser = withAuthentication(User);
+import { ConnectedApplications } from './HeaderComponents/Applications';
+import { ConnectedUser } from './HeaderComponents/User';
 
 class Header extends Component {
 
@@ -98,11 +17,11 @@ class Header extends Component {
         return <header className="ui-dashboard-header">
             <DashboardMenu
                 left={[
-                    { headerLabel: 'Applications', bodyItem: ApplicationsRouter, name: 'applications' },
+                    { headerLabel: 'Applications', bodyItem: ConnectedApplications, name: 'applications' },
                 ]}
                 center={<Link className="dashboard-menu__header__link" to="/">Dashboard</Link>}
                 right={[
-                    { headerLabel: userLabel, bodyItem: connectedUser, name: 'user', showQueryInput: false, floatingControls: true }
+                    { headerLabel: userLabel, bodyItem: ConnectedUser, name: 'user', showQueryInput: false, floatingControls: true }
                 ]}
             />
         </header>;
