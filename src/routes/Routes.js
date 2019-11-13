efc94f0358c53d19359aa9e907a799e3024b0140
grasshousedev <finnelyhawthorne@gmail.com>
2019-11-13 13:51:29 +0000

    Added rest and Tag to Badge ESlint changes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 import { Home } from 'pages/Home';
 import { Landing as MachineLearningLanding } from 'apps/machine-learning/Landing';
 import { StyleShowcase } from 'apps/style-showcase/StyleShowcase';
 import { FinanceLanding, FINANCE_BASE_URL } from 'apps/dashboard-finance/app/FinanceLanding';
+
 
 function Routes() {
     return <Switch>
@@ -12,7 +13,7 @@ function Routes() {
         <Route path={`${FINANCE_BASE_URL}`} component={FinanceLanding} />
         <Route path="/machine-learning" component={MachineLearningLanding} />
         <Route path="/style-showcase" component={StyleShowcase} />
-    </Switch>;        
+    </Switch>;
 }
 
 const ConnectedRoutes = withRouter(Routes);
