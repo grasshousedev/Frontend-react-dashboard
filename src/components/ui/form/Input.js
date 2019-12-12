dba4433abfff778dfbd7b05dd02b01f6d49b830e
grasshousedev <finnelyhawthorne@gmail.com>
2019-12-12 17:54:56 +0000

    Added invalid to Input and Select

--- src/components/ui/form/Input.js
+++ src/components/ui/form/Input.js
@@ -5,14 +5,20 @@ import { FORM_BASE_CLASS } from './Form';
 
 
 const FORM_INPUT_CLASS = `${FORM_BASE_CLASS}__field__input`;
+const FORM_INPUT_DISABLED_CLASS = `${FORM_INPUT_CLASS}--disabled`;
+const FORM_INPUT_INVALID_CLASS = `${FORM_INPUT_CLASS}--invalid`;
 
 
-export function Input({ className, ...rest }) {
-    const inputClassName = `${FORM_INPUT_CLASS} ${className}`;
+export function Input({ className, disabled, invalid, ...rest }) {
+    const disabledClass = disabled ? FORM_INPUT_DISABLED_CLASS : '';
+    const invalidClass = invalid ? FORM_INPUT_INVALID_CLASS : '';
+    const inputClassName = `${FORM_INPUT_CLASS} ${disabledClass} ${invalidClass} ${className}`;
 
-    return <input className={inputClassName} {...rest} />;
+    return <input className={inputClassName} disabled={disabled} {...rest} />;
 }
 
 Input.propTypes = {
     className: PropTypes.string,
+    disabled: PropTypes.bool,
+    invalid: PropTypes.any,
 };
--- src/components/ui/form/Select.js
+++ src/components/ui/form/Select.js
@@ -5,8 +5,14 @@ import Select from 'react-select';
 import { FORM_BASE_CLASS } from './Form';
 
 
-function FormSelect({ options, extraClassName, value, key='value', ...rest }) {
-    const selectClassName = `${FORM_BASE_CLASS}__react-select ${extraClassName || ''}`;
+const SELECT_CLASS = `${FORM_BASE_CLASS}__react-select`;
+const SELECT_INVALID_CLASS = `${SELECT_CLASS}--invalid`;
+
+
+function FormSelect({ options, className='', value, invalid, key='value', ...rest }) {
+    const invalidClass = invalid ? SELECT_INVALID_CLASS : '';
+    const selectClassName = `${SELECT_CLASS} ${invalidClass} ${className}`;
+
     const mappedOptions = useMemo(() =>
         options.reduce((all, o) => { all[o[key]] = o; return all; }, {}),
         [options, key]
@@ -25,8 +31,9 @@ function FormSelect({ options, extraClassName, value, key='value', ...rest }) {
 FormSelect.propTypes = {
     options: PropTypes.array.isRequired,
     value: PropTypes.any,
+    invalid: PropTypes.any,
     key: PropTypes.string,
-    extraClassName: PropTypes.string,
+    className: PropTypes.string,
 };
 
 export { FormSelect as Select };
