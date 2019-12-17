048d7f751ba5db56191f7e313b2d98ebb7b6b242
grasshousedev <finnelyhawthorne@gmail.com>
2019-12-17 10:40:53 +0000

    Added className to Form

--- src/components/ui/form/Form.js
+++ src/components/ui/form/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { propTypeChildren } from 'components/utils';
 
 export const SPACEBAR_CODE = 32;
@@ -8,14 +8,15 @@ export const FORM_BASE_CLASS = 'ui-form';
 const CONTAINER_CLASS = `${FORM_BASE_CLASS}__container`;
 
 
-export function Form({ children }) {
-    const formClassName = `${FORM_BASE_CLASS} ${CONTAINER_CLASS}`;
+export function Form({ children, className='', ...rest }) {
+    const formClassName = `${FORM_BASE_CLASS} ${CONTAINER_CLASS} ${className}`;
 
-    return <div className={formClassName}>
+    return <div className={formClassName} {...rest}>
         {children}
     </div>;
 }
 
 Form.propTypes = {
     children: propTypeChildren,
+    className: PropTypes.string,
 };
