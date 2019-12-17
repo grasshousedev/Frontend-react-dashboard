9b6f914ca273c7b7bbea8587bc9663f0e9448b81
grasshousedev <finnelyhawthorne@gmail.com>
2019-12-17 10:41:12 +0000

    Improved Select with Tag specification

--- src/components/ui/form/Select.js
+++ src/components/ui/form/Select.js
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
+import CreatableSelect from 'react-select/creatable';
 
 import { FORM_BASE_CLASS } from './Form';
 
@@ -9,7 +10,8 @@ const SELECT_CLASS = `${FORM_BASE_CLASS}__react-select`;
 const SELECT_INVALID_CLASS = `${SELECT_CLASS}--invalid`;
 
 
-function FormSelect({ options, className='', value, invalid, key='value', ...rest }) {
+function FormSelect({ options, className='', value, invalid=false, disabled=false, key='value', isMulti=false, creatable=false, ...rest }) {
+    const Tag = creatable ? CreatableSelect : Select;
     const invalidClass = invalid ? SELECT_INVALID_CLASS : '';
     const selectClassName = `${SELECT_CLASS} ${invalidClass} ${className}`;
 
@@ -17,13 +19,17 @@ function FormSelect({ options, className='', value, invalid, key='value', ...res
         options.reduce((all, o) => { all[o[key]] = o; return all; }, {}),
         [options, key]
     );
-    const selectValue = mappedOptions[value];
+    const selectValue = isMulti
+        ? value.map(v => mappedOptions[v] || { [key]: v, label: v })
+        : value ? mappedOptions[value] : null;
 
-    return <Select
+    return <Tag
         className={selectClassName}
         classNamePrefix="react-select"
         options={options}
         value={selectValue}
+        isDisabled={disabled}
+        isMulti={isMulti}
         {...rest}
     />;
 }
@@ -32,8 +38,11 @@ FormSelect.propTypes = {
     options: PropTypes.array.isRequired,
     value: PropTypes.any,
     invalid: PropTypes.any,
+    disabled: PropTypes.bool,
     key: PropTypes.string,
     className: PropTypes.string,
+    isMulti: PropTypes.bool,
+    creatable: PropTypes.bool,
 };
 
 export { FormSelect as Select };
