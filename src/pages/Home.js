cc523829fdf382ada5bc65354027ffbed453b63f
grasshousedev <finnelyhawthorne@gmail.com>
2019-12-20 16:07:30 +0000

    Added luas and Finance widgets to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,39 @@
 import React from 'react';
+
 import { Lorem } from 'components/ui/Lorem';
+import { PageBody } from 'components/ui/PageBody';
+import { RowBlock, ColumnBlock, Block } from 'components/ui/Blocks';
+
+import { FinanceWidgets } from './FinanceWidgets';
+
+import { LuasForecast,  } from 'apps/luas/src/components/LuasForecast';
+import { LuasMultiForecast } from 'apps/luas/src/components/LuasMultiForecast';
+import { STATIONS } from 'apps/luas/src/constants';
+import { mockRequests } from 'apps/luas/tests/mock';
+mockRequests();
+
 
 export function Home() {
-    return <div className="ui-page__body">
-        <h1>Dashboard Home</h1>
-        <div>Welcome to my Dashboard.</div>
-
-        <Lorem paragraphs={3} />
-    </div>;
+    return <PageBody fullHeight={true}>
+        <RowBlock>
+            <ColumnBlock className="col-xs-12 col-md-4">
+                <h1 style={{ textAlign: 'center' }}>LUAS</h1>
+                <Block isOutstanding={true}>
+                    <LuasForecast station={STATIONS.GAL} />
+                </Block>
+                <Block isOutstanding={true}>
+                    <LuasMultiForecast stations={[STATIONS.DAW, STATIONS.STS, STATIONS.HAR]} />
+                </Block>
+            </ColumnBlock>
+            <ColumnBlock className="col-xs-12 col-md-4">
+                <h1 style={{ textAlign: 'center' }}>Finance</h1>
+                <FinanceWidgets />
+            </ColumnBlock>
+            <ColumnBlock className="col-xs-12 col-md-4">
+                <h1>Dashboard Home</h1>
+                <div>Welcome to my Dashboard.</div>
+                <Lorem paragraphs={3} />
+            </ColumnBlock>
+        </RowBlock>
+    </PageBody>;
 }
