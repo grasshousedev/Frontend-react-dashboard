4c7b30bab3f792c36e5993eb0f26e09528274cb0
grasshousedev <finnelyhawthorne@gmail.com>
2020-01-04 16:37:11 +0000

    Added spacebar as click for icon clicks

--- src/components/ui/Icon.js
+++ src/components/ui/Icon.js
@@ -7,7 +7,7 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 
 const UI_ICON_CLASS = 'ui-icon';
-
+const SPACEBAR_CODE = 32;
 
 export function Icon({ name, size='normal', modifiers=[], className='', ...rest }) {
     const sizeClass = `${UI_ICON_CLASS}--${size}`;
@@ -31,7 +31,17 @@ Icon.propTypes = {
 export function IconControl({ className='', ...rest }) {
     const iconControlClassName = `${className} ui-icon__control`;
 
-    return <Icon size="smaller" className={iconControlClassName} {...rest} />;
+    const onKeyDown = rest.onKeyDown ? rest.onKeyDown : rest.onClick
+        ? e => {
+            if (e.keyCode === SPACEBAR_CODE) {
+                e.preventDefault();
+                e.stopPropagation();
+                rest.onClick();
+            }
+        }
+        : undefined;
+
+    return <Icon size="smaller" className={iconControlClassName} onKeyDown={onKeyDown} {...rest} />;
 }
 
 IconControl.propTypes = {
